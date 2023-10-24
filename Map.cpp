c155756f1ef83d8ccacbe463336b69426419b69e
mvsyr <123527909+mvsyr@users.noreply.github.com>
2023-10-23 21:58:28 -0400

    test draft

---
+++ Map.cpp
@@ -0,0 +1,23 @@
+﻿// Map.cpp : Defines the entry point for the application.
+//
+
+#include "Map.h"
+
+using namespace std;
+
+void map(const string& filename, OutIter output) {
+	ifstream infile(filename.c_str());
+		while (infile.good()) {
+			string line;
+				getline(infile, line);
+				regex word_re("(\\w+)");
+				sregex_iterator iter(line.begin(), line.end(), word_re);
+				sregex_iterator end;
+				for (; iter != end; ++iter) {
+					string word = iter­ > str();
+						output = std::pair<string, int>(word, 1);
+				}
+		}
+	infile.close();
+}
+};

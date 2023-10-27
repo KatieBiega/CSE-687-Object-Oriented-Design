ea3c34be2a4a3c3a1a93604a739aa04417e4ac48
KatieBiega <140035089+KatieBiega@users.noreply.github.com>
2023-10-27 00:09:10 -0400

    Update File Management Class.cpp

--- File Management Class.cpp
+++ File Management Class.cpp
@@ -1,73 +1,41 @@
+#include "File Management.h"
 #include <iostream>
 #include <fstream>
-#include <string>
-#include <vector>
 
 using namespace std;
 using std::cout;
 using std::cin;
 using std::endl;
 
-class fileManagement {
-public:
-    fileManagement(const string& inputDir, const string& outputDir, const string& tempDir)
-        : inputDirectory(inputDir), outputDirectory(outputDir), tempDirectory(tempDir) {}
+FileManagement::FileManagement(const std::string& inputDir, const std::string& outputDir, const std::string& tempDir)
+    : inputDirectory(inputDir), outputDirectory(outputDir), tempDirectory(tempDir) {}
 
-    // Read the contents of a file and return it as a string
-    string ReadFile(const std::string& fileName) {
-        ifstream file(inputDirectory + "/" + fileName);
-        if (file.is_open()) {
-            string content((istreambuf_iterator<char>(file)), (istreambuf_iterator<char>()));
-            file.close();
-            return content;
-        }
-        else {
-            // Handle file not found or other errors
-            cerr << "Error: Unable to read file " << fileName << endl;
-            return "";
-        }
+string FileManagement::ReadFile(const std::string& fileName) {
+    ifstream file(inputDirectory + "/" + fileName);
+    if (file.is_open()) {
+        std::string content((istreambuf_iterator<char>(file)), (istreambuf_iterator<char>()));
+        file.close();
+        return content;
     }
-
-    // Write data to a temporary file
-    void WriteToTempFile(const string& fileName, const string& data) {
-        ofstream file(tempDirectory + "/" + fileName);
-        if (file.is_open()) {
-            file << data;
-            file.close();
-        }
-        else {
-            // Handle error
-            cerr << "Error: Unable to write to temporary file " << fileName << endl;
-        }
+    else {
+        // Handle file not found or other errors
+        cerr << "Error: Unable to read file " << fileName << endl;
+        return "";
     }
+}
 
-    // Move a temporary file to the output directory
-    void MoveToOutput(const string& tempFileName, const string& outputFileName) {
-        // Implement file move operation (e.g., using std::filesystem)
+void FileManagement::WriteToTempFile(const string& fileName, const string& data) {
+    ofstream file(tempDirectory + "/" + fileName);
+    if (file.is_open()) {
+        file << data;
+        file.close();
     }
-
-    // Other file management operations can be added as needed
-
-private:
-    string inputDirectory;
-    string outputDirectory;
-    string tempDirectory;
-};
-
-int main(int argc, char** argv) {
-    
-    
-    if (argc != 4) {
-        std::cerr << "Usage: program input_directory output_directory temp_directory" << endl;
-        return 1;
+    else {
+        
+        cerr << "Error: Unable to write to temporary file " << fileName << endl;
     }
+}
 
-    fileManagement fileManager(argv[1], argv[2], argv[3]);
-
-    // Example usage:
-    string content = fileManager.ReadFile("input.txt");
-    fileManager.WriteToTempFile("temp.txt", content);
-    fileManager.MoveToOutput("temp.txt", "output.txt");
-
-    return 0;
+void FileManagement::MoveToOutput(const string& tempFileName, const string& outputFileName) {
+    // Implement file move operation (e.g., using std::filesystem)
 }

6f90e88e92eddb7f9571f18c007dce50c6b94873
mvsyr <123527909+mvsyr@users.noreply.github.com>
2023-10-28 20:26:48 -0400

    Add files via upload

--- Map.cpp
+++ Map.cpp
@@ -1,23 +1,41 @@
-﻿// Map.cpp : Defines the entry point for the application.
-//
-
+﻿#include <iostream>
+#include <fstream>
+#include <map>
+#include <string>
+#include <algorithm>
+#include <cctype>
 #include "Map.h"
 
-using namespace std;
+using std::string;
+using std::ifstream;
 
-void map(const string& filename, OutIter output) {
-	ifstream infile(filename.c_str());
-		while (infile.good()) {
-			string line;
-				getline(infile, line);
-				regex word_re("(\\w+)");
-				sregex_iterator iter(line.begin(), line.end(), word_re);
-				sregex_iterator end;
-				for (; iter != end; ++iter) {
-					string word = iter­ > str();
-						output = std::pair<string, int>(word, 1);
-				}
+typedef std::map<std::string, int> StrIntMap;
+
+void map::map(const string& filename, string& line)
+{
+	//read in all the words
+	StrIntMap word_count;
+	
+	while (filename >> line)
+	{
+		//remove punctuation
+		while ((int index = line.find_first_of(".,:!?\\;-*+[]<>() '")) != string::npos)
+		{
+			line.erase(index, 1);
 		}
-	infile.close();
+		//set to lowercase 
+		std::transform(line.begin(), line.end(), line.begin(),
+			[](unsigned char c) { return std::tolower(c); });
+		//increase map word count
+		++word_count[word];
+	}
 }
-};
+
+void map::export(map word_count)
+{
+	for (word_count::iterator p = word_count.begin(); p != word_count.end(); ++p) {
+		std::cout << "(\"" << p->first << "\"," << p->second << ")\n";
+	}
+	//write to file
+
+}
---
+++ Map.h
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <fstream>
+#include <map>
+#include <string>
+#include <vector>
+
+class map {
+public:
+
+	void map();
+	void export();
+
+private: 
+
+	//
+};

d14438531849886a61a35efc12ec4fb9aa516152
mvsyr <123527909+mvsyr@users.noreply.github.com>
2023-10-30 19:50:08 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -21,14 +21,14 @@ using std::cin;
 
 
 int main() {
+
+	//String initialization
 	string fileName = "";
 	string fileString = "";
 	string inputDirectory = "";
 	string outputDirectory = "";
 	string tempDirectory = "";
 
-	Reduce Reduction;
-	Map Mapping;
 	string mapped_string;
 	string tempFilename = "TempFile.txt";
 	string tempFileContent;
@@ -37,8 +37,12 @@ int main() {
 	string successString = "";
 	string successFilename = "SUCCESS.txt";
 
+	//Class object creation
+	Reduce Reduction;
+	Map Mapping;
+
 	//EXECUTIVE//
-	//Prompt for input
+	//Prompt for input of file directories
 	cout << "==== MAP & REDUCE ====\n\n";
 
 	cout << "Enter the input directory: "; // this should contain the files for raw data input
@@ -52,17 +56,20 @@ int main() {
 	//Create file management class based on the user inputs
 	FileManagement FileManage(inputDirectory, outputDirectory, tempDirectory);
 
+	//Read all files into single string and pass to Map class
 	cout << "FileManagement Class initialized.\n";
-
 	fileString = FileManage.ReadAllFiles(); 
 	cout << "All files read.\n";
 	Mapping.map(fileString);
 	cout << "String from files passed to map function.\n";
 
-	mapped_string = Mapping.string_export();
+	//Write mapped output string to intermediate file 
+	mapped_string = Mapping.mapped_export();
 	cout << "Mapping complete; exporting resulting string.\n";
 	FileManage.WriteToTempFile(tempFilename, mapped_string);
 	cout << "String from mapping written to temp file.\n";
+
+	//Read from intermediate file and pass data to Reduce class
 	tempFileContent = FileManage.ReadFromTempFile(tempFilename);
 	cout << "New string read from temp file.\n";
 	Reduction.import(tempFileContent);
@@ -76,6 +83,7 @@ int main() {
 	reduced_string = Reduction.reduce_export();
 	cout << "Vector exported to string.\n";
 
+	//Sorted, aggregated, and reduced output string is written into final output file
 	FileManage.WriteToOutputFile(outputFilename, reduced_string);
 	cout << "string written to output file.\n";
 	FileManage.WriteToOutputFile(successFilename, successString);
